573ceb846857854a7334a49f7a9d4f3dd8e47938
RobertoDanielBrusco <bruscor@gmail.com>
2024-03-05 10:30:10 -0300

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        verdeNucleo: '#7EA940',
+        nucleo: '#90C447',
+        verdeCheck: '#506F20',
+        bgNav: '#E8EEE0',
+        bgIcons: '#EEF4F8',
+        bgVerdeClaro: '#e8eee0',
+        borderColor: '#9994',
+        verdeSubtitles: '#D9E4C9',
+        stars: '#fdb900',
+        verdePrices: '#cce2aa',
+      },
+    },
+  },
+  plugins: [],
+};
